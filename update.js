5ae39b2c00170e51708e93f17ca296ea498191b2
Hannah Stahl <hls62@cornell.edu>
2019-02-01 17:12:07 -0600

    Reverted debugging change

--- update.js
+++ update.js
@@ -26,6 +26,6 @@ export async function main(event, context) {
     await dynamoDbLib.call("update", params);
     return success({ status: true });
   } catch (e) {
-    return failure({ status: e });
+    return failure({ status: false });
   }
 }
